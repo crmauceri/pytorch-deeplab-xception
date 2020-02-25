4a45963a1af63746a4d666433d9fc044eefeb80d
Cecilia <cema3961@colorado.edu>
2020-02-25 14:48:33 -0800

    Add optional name argument for use with detectron2

--- deeplab3/modeling/backbone/__init__.py
+++ deeplab3/modeling/backbone/__init__.py
@@ -1,14 +1,16 @@
 from deeplab3.modeling.backbone import resnet, xception, drn, mobilenet
 
-def build_backbone(cfg, BatchNorm): #backbone, output_stride, BatchNorm, use_depth, use_deeplab_format=True):
+def build_backbone(cfg, BatchNorm, name=None): #backbone, output_stride, BatchNorm, use_depth, use_deeplab_format=True):
 
-    if cfg.MODEL.BACKBONE == 'resnet':
+    if name is None:
+        name = cfg.MODEL.BACKBONE
+    if name == 'resnet':
         return resnet.ResNet101(cfg, BatchNorm)
-    elif cfg.MODEL.BACKBONE == 'xception':
+    elif name == 'xception':
         return xception.AlignedXception(cfg, BatchNorm)
-    elif cfg.MODEL.BACKBONE == 'drn':
+    elif name == 'drn':
         return drn.drn_d_54(BatchNorm)
-    elif cfg.MODEL.BACKBONE == 'mobilenet':
+    elif name == 'mobilenet':
         return mobilenet.MobileNetV2(cfg, BatchNorm)
     else:
         raise NotImplementedError

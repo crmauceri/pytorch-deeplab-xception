faf030d2d95604f11d7b8eba5ec20507e4982d7d
Cecilia <cema3961@colorado.edu>
2020-04-30 14:29:32 -0700

    Call the depthaware network

--- deeplab3/modeling/backbone/__init__.py
+++ deeplab3/modeling/backbone/__init__.py
@@ -1,4 +1,5 @@
 from deeplab3.modeling.backbone import resnet, xception, drn, mobilenet
+from deeplab3.modeling.backbone.depthawarecnn import depth_aware_resnet
 
 def build_backbone(cfg, BatchNorm, name=None): #backbone, output_stride, BatchNorm, use_depth, use_deeplab_format=True):
 
@@ -12,5 +13,7 @@ def build_backbone(cfg, BatchNorm, name=None): #backbone, output_stride, BatchNo
         return drn.drn_d_54(BatchNorm)
     elif name == 'mobilenet':
         return mobilenet.MobileNetV2(cfg, BatchNorm)
+    elif name == 'depthaware_resnet':
+        return depth_aware_resnet.ResNet101(cfg, BatchNorm)
     else:
         raise NotImplementedError

7fcd91d1d644767518b82cf4215fad00273d8d3f
Cecilia <cema3961@colorado.edu>
2020-10-17 13:50:03 -0700

    Missing variable

--- visualization_scripts/run_all_models.py
+++ visualization_scripts/run_all_models.py
@@ -4,7 +4,7 @@ import model_utils
 import json
 import traceback
 
-def run_model(cfg, result_file, rerun=False):
+def run_model(cfg, cfg_filepath, result_file, rerun=False):
     try:
         cfg.merge_from_list(['CHECKPOINT.DIRECTORY', os.path.dirname(cfg_filepath),
                              'TEST.MAX_ITER', 1000,
@@ -35,7 +35,7 @@ def run_all_models(models, rerun=False):
     for cfg_filepath in models:
         cfg = model_utils.match_cfg_versions(cfg_filepath)
         result_file = os.path.join(cfg.CHECKPOINT.DIRECTORY, 'validation_report.txt')
-        if not run_model(cfg, result_file, rerun):
+        if not run_model(cfg, cfg_filepath, result_file, rerun):
             failed.append(cfg_filepath)
 
     print("Failed models: ".format("\n".join(failed)))
@@ -55,7 +55,7 @@ def run_low_light_models(low_light_models, gain, gamma, rerun=False):
                 result_file = os.path.join(cfg.CHECKPOINT.DIRECTORY,
                                            'validation_report_gain{:3.2f}_gamma{:3.2f}.txt'.format(float(i), float(j)))
 
-                if not run_model(cfg, result_file, rerun):
+                if not run_model(cfg, cfg_filepath, result_file, rerun):
                     failed.append(cfg_filepath)
 
     print("Failed models: ".format("\n".join(failed)))

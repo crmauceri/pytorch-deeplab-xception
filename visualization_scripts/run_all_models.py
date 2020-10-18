6d37bd5107e1d81f826b32c6499badd8567b0589
Cecilia <cema3961@colorado.edu>
2020-10-17 18:43:09 -0700

    Add try/except block for config mismatch

--- visualization_scripts/run_all_models.py
+++ visualization_scripts/run_all_models.py
@@ -36,10 +36,15 @@ def run_all_models(models, rerun=False):
     failed = []
 
     for cfg_filepath in models:
-        cfg = model_utils.match_cfg_versions(cfg_filepath)
-        checkpoint_dir = os.path.dirname(cfg_filepath)
-        result_file = os.path.join(checkpoint_dir, 'validation_report.txt')
-        if not run_model(cfg, cfg_filepath, result_file, rerun):
+        try:
+            cfg = model_utils.match_cfg_versions(cfg_filepath)
+            checkpoint_dir = os.path.dirname(cfg_filepath)
+            result_file = os.path.join(checkpoint_dir, 'validation_report.txt')
+            if not run_model(cfg, cfg_filepath, result_file, rerun):
+                failed.append(cfg_filepath)
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
             failed.append(cfg_filepath)
 
     print("Failed models: ".format("\n".join(failed)))
@@ -51,16 +56,21 @@ def run_low_light_models(low_light_models, gain, gamma, rerun=False):
     for cfg_filepath in low_light_models:
         for i in gain:
             for j in gamma:
-                cfg = model_utils.match_cfg_versions(cfg_filepath)
-                cfg.merge_from_list(['DATASET.DARKEN.DARKEN', True,
-                                     'DATASET.DARKEN.GAIN', float(i),
-                                     'DATASET.DARKEN.GAMMA', float(j)])
+                try:
+                    cfg = model_utils.match_cfg_versions(cfg_filepath)
+                    cfg.merge_from_list(['DATASET.DARKEN.DARKEN', True,
+                                         'DATASET.DARKEN.GAIN', float(i),
+                                         'DATASET.DARKEN.GAMMA', float(j)])
 
-                checkpoint_dir = os.path.dirname(cfg_filepath)
-                result_file = os.path.join(checkpoint_dir,
-                                           'validation_report_gain{:3.2f}_gamma{:3.2f}.txt'.format(float(i), float(j)))
+                    checkpoint_dir = os.path.dirname(cfg_filepath)
+                    result_file = os.path.join(checkpoint_dir,
+                                               'validation_report_gain{:3.2f}_gamma{:3.2f}.txt'.format(float(i), float(j)))
 
-                if not run_model(cfg, cfg_filepath, result_file, rerun):
+                    if not run_model(cfg, cfg_filepath, result_file, rerun):
+                        failed.append(cfg_filepath)
+                except Exception as e:
+                    print(e)
+                    traceback.print_exc()
                     failed.append(cfg_filepath)
 
     print("Failed models: ".format("\n".join(failed)))
@@ -76,16 +86,15 @@ if __name__ == "__main__":
     model_configs = model_utils.get_all_models("../run/coco/")
     run_all_models(model_configs, True)
 
-    # low_light_models = ['../run/older/cityscapes_rgbd_xception_fine/2020_04_26-20_04_07/parameters.txt.yaml',
-    #                     '../run/older/cityscapes_rgb_xception_pt/2020_04_27-01_43_58/parameters.txt.yaml',
-    #                     '../run/cityscapes/cityscapes_rgbd_xception_low_light/2020_09_25-19_32_43/parameters.yaml',
-    #                     '../run/cityscapes/cityscapes_rgb_xception_low_light/2020_09_25-19_36_53/parameters.yaml',
-    #                     '../run/scenenet/scenenet_rgbd_xception/2020_09_17-22_10_19/parameters.yaml',
-    #                     '../run/scenenet/scenenet_rgb_xception/2020_09_17-22_14_43/parameters.yaml',
-    #                     '../run/scenenet/scenenet_rgbd_xception_low_light/2020_09_25-23_11_51/parameters.yaml',
-    #                     '../run/scenenet/scenenet_rgbd_xception_low_light/2020_09_28-08_36_05/parameters.yaml']
-    #
-    # gain = np.linspace(0.1, 1, 3).tolist()
-    # gamma = np.linspace(1,3, 3).tolist()
-    #
-    # run_low_light_models(low_light_models, gain, gamma)
+    low_light_models = ['../run/cityscapes/cityscapes_rgbd_xception_fine_coarse/2020_08_20-15_58_16/parameters.txt.yaml',
+                       '../run/cityscapes/cityscapes_rgb_xception_pt_fine_coarse/2020_08_03-15_41_22/parameters.txt.yaml',
+                        '../run/cityscapes/cityscapes_rgbd_xception_low_light/2020_09_25-19_32_43/parameters.yaml',
+                        '../run/cityscapes/cityscapes_rgb_xception_low_light/2020_09_25-19_36_53/parameters.yaml',
+                        '../run/scenenet/scenenet_rgbd_xception/2020_09_17-22_10_19/parameters.yaml',
+                        '../run/scenenet/scenenet_rgb_xception/2020_09_17-22_14_43/parameters.yaml',
+                        '../run/scenenet/scenenet_rgbd_xception_low_light/2020_09_25-23_11_51/parameters.yaml',
+                        '../run/scenenet/scenenet_rgbd_xception_low_light/2020_09_28-08_36_05/parameters.yaml']
+
+    gain = [0.33, 0.66, 1.0]
+    gamma = [1.0, 2.0, 3.0]
+    run_low_light_models(low_light_models, gain, gamma, True)
